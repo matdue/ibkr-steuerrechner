13107ad0c2182029e76761d0bc67804348c126c7
Matthias Düsterhöft <m.duesterhoeft@gmail.com>
2023-06-25 18:16:08 +0200

    Finalized dividend calculation

--- src/app.py
+++ src/app.py
@@ -1,21 +1,26 @@
 import io
 import locale
+from typing import Optional
 
 import pandas as pd
 import streamlit as st
 
 
-def categorize_statement_record(record: pd.Series):
+def categorize_statement_record(record: pd.Series) -> tuple[str, Optional[str]]:
     if record["Description"] in ["Opening Balance", "Closing Balance"]:
-        return "balance"
+        return "balance", None
 
     if record["Description"] == "Electronic Fund Transfer":
-        return "transfer"
+        return "transfer", None
 
     if "Cash Dividend" in record["Description"]:
-        return "dividend"
+        if record["Description"].endswith(" (Ordinary Dividend)"):
+            return "dividend", "ordinary"
+        if record["Description"].endswith(" Tax"):
+            return "dividend", "tax"
+        return "dividend", "other"
 
-    return "other"
+    return "other", None
 
 def read_statement_file(file: io.TextIOBase):
     """
@@ -41,9 +46,10 @@ def read_statement_file(file: io.TextIOBase):
     df = pd.read_csv(statement_of_funds_lines, parse_dates=["Report Date", "Activity Date"])
     df["Debit"] = pd.to_numeric(df["Debit"], errors="coerce")
     df["Credit"] = pd.to_numeric(df["Credit"], errors="coerce")
+    df["Sum"] = df[["Debit", "Credit"]].sum(axis=1)
     df["Balance"] = pd.to_numeric(df["Balance"], errors="coerce")
     df["Report_Year"] = df["Report Date"].dt.year.astype("str")
-    df["Activity_Year"] = df["Activity Date"].dt.year.astype("str")
+    df["Activity_Year"] = df["Activity Date"].apply(lambda d: None if pd.isnull(d) else str(d.year))
 
     # Skip all records which are not in base currency
     df = df.query("Currency == 'Base Currency Summary'").drop(columns=["Statement of Funds", "Header", "Currency"])
@@ -51,16 +57,60 @@ def read_statement_file(file: io.TextIOBase):
     return df
 
 
+def display_fund_transfer(df_year: pd.DataFrame):
+    st.header("Ein- und Auszahlungen")
+    df_transfer = df_year.query("Category == 'transfer'")
+    deposited_funds = df_transfer["Credit"].sum()
+    withdrawn_funds = df_transfer["Debit"].sum()
+    st.write(f"Einzahlungen: {locale.currency(deposited_funds, grouping=True)}")
+    st.write(f"Auszahlungen: {locale.currency(withdrawn_funds, grouping=True)}")
+    with st.expander("Auszug"):
+        st.dataframe(df_transfer)
+
+
+def display_dividends(df_year: pd.DataFrame, df_year_corrections: pd.DataFrame, selected_year: str):
+    st.header("Dividenden")
+    df_dividend = df_year.query("Category == 'dividend'")
+    df_dividend_corrections = df_year_corrections.query("Category == 'dividend'")
+    ordinary_dividends = df_dividend.query("Subcategory == 'ordinary'")
+    ordinary_dividends_corrections = df_dividend_corrections.query("Subcategory == 'ordinary'")
+    earned_dividends = ordinary_dividends["Sum"].sum()
+    earned_dividends_corrections = ordinary_dividends_corrections["Sum"].sum()
+
+    taxes = df_dividend.query("Subcategory == 'tax'")
+    taxes_corrections = df_dividend_corrections.query("Subcategory == 'tax'")
+    withheld_taxes = taxes["Sum"].sum()
+    withheld_taxes_corrections = taxes_corrections["Sum"].sum()
+
+    others = df_dividend.query("Subcategory == 'other'")
+    others_corrections = df_dividend_corrections.query("Subcategory == 'other'")
+    other_dividends = others["Sum"].sum()
+    other_dividends_corrections = others_corrections["Sum"].sum()
+
+    st.subheader("Gewöhnliche Dividenden")
+    if earned_dividends_corrections:
+        st.write(f"Summe {selected_year}: {locale.currency(earned_dividends, grouping=True)}")
+        st.write(f"Korrektur: {locale.currency(earned_dividends_corrections, grouping=True)}")
+    st.write(f"Summe: {locale.currency(earned_dividends+earned_dividends_corrections, grouping=True)}")
+
+    st.subheader("Quellensteuer")
+    if withheld_taxes_corrections:
+        st.write(f"Summe {selected_year}: {locale.currency(withheld_taxes * (-1), grouping=True)}")
+        st.write(f"Korrektur: {locale.currency(withheld_taxes_corrections * (-1), grouping=True)}")
+    st.write(f"Summe: {locale.currency((withheld_taxes+withheld_taxes_corrections) * (-1), grouping=True)}")
+
+    if other_dividends or other_dividends_corrections:
+        st.subheader("Andere Zahlungen")
+        st.write(f"Summe {selected_year}: {locale.currency(other_dividends, grouping=True)}")
+        st.write(f"Korrektur: {locale.currency(other_dividends_corrections, grouping=True)}")
+        st.write(f"Summe: {locale.currency(other_dividends+other_dividends_corrections, grouping=True)}")
+
+    with st.expander("Auszug"):
+        st.dataframe(pd.concat([df_dividend, df_dividend_corrections]))
+
+
 def main():
-    # TODO: Im Dockerfile: RUN locale-gen en_US.UTF-8
-    try:
-        locale.setlocale(locale.LC_ALL, "de_DE.utf8")
-    except locale.Error:
-        st.write("locale de_DE.utf8 unavailable")
-        try:
-            locale.setlocale(locale.LC_ALL, "de_DE")
-        except locale.Error:
-            st.write("locale de_DE unavailable")
+    locale.setlocale(locale.LC_ALL, "de_DE.utf8")
 
     st.title("Steuerrechner für Interactive Brokers")
     st.caption("Zur Berechnung der Steuerschuld von Optionen und Aktiengeschäften")
@@ -81,27 +131,15 @@ def main():
     if selected_year == years_options[0]:
         return
 
-    st.write(f"Berechnung läuft für das Jahr {selected_year}...")
-    df["Category"] = df.apply(categorize_statement_record, axis=1).astype("category")
-    df_year = df.query("Report_Year == @selected_year")
-    st.dataframe(df_year)
+    st.header(f"Ergebnis für das Jahr {selected_year}")
+    df[["Category", "Subcategory"]] = df.apply(categorize_statement_record, axis=1, result_type="expand")
+    df_year = df.query("Report_Year == @selected_year and Activity_Year == @selected_year")
+    df_year_corrections = df.query("Report_Year > @selected_year and Activity_Year == @selected_year")
+    with st.expander("Kompletter Kontoauszug"):
+        st.dataframe(df)
 
-    st.header("Ein- und Auszahlungen")
-    df_transfer = df_year.query("Category == 'transfer'")
-    deposited_funds = df_transfer.filter(["Credit"]).sum()[0]
-    withdrawn_funds = df_transfer.filter(["Debit"]).sum()[0]
-    st.write(f"Einzahlungen: {locale.currency(deposited_funds, grouping=True)}")
-    st.write(f"Auszahlungen: {locale.currency(withdrawn_funds, grouping=True)}")
-    st.dataframe(df_transfer)
-
-    st.header("Dividenden")
-    # FIXME: Was ist mit Steuerkorrekturen aus dem letzten Jahr?
-    df_dividend = df_year.query("Category == 'dividend'")
-    earned_dividends = df_dividend.filter(["Credit"]).sum()[0]
-    withheld_taxes = abs(df_dividend.filter(["Debit"]).sum()[0])
-    st.write(f"Dividendenzahlungen: {locale.currency(earned_dividends, grouping=True)}")
-    st.write(f"Quellensteuer: {locale.currency(withheld_taxes, grouping=True)}")
-    st.dataframe(df_dividend)
+    display_fund_transfer(df_year)
+    display_dividends(df_year, df_year_corrections, selected_year)
 
 
 if __name__ == "__main__":
